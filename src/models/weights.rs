8563caea2c1db7075349d06fd671cb9a17c63f0e
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-12-06 11:34:32 -0600

    Remove non-utilized code

--- src/models/weights.rs
+++ src/models/weights.rs
@@ -6,21 +6,3 @@ pub struct Weights {
     pub match_score_threshold: f32,
     pub symbol_abbr_threshold: f32,
 }
-
-impl Weights {
-    pub fn new(
-        continuity: f32,
-        mismatched_letter_penalty: f32,
-        mismatched_word_penalty: f32,
-        match_score_threshold: f32,
-        symbol_abbr_threshold: f32,
-    ) -> Self {
-        Self {
-            continuity,
-            mismatched_letter_penalty,
-            mismatched_word_penalty,
-            match_score_threshold,
-            symbol_abbr_threshold,
-        }
-    }
-}
