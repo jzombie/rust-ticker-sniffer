2fbc01e6400c60b56fac21abe0ae57c49b177761
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-12-07 20:36:51 -0600

    Add result bias adjuster to bench

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -3,7 +3,10 @@ mod test_utils;
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use test_utils::load_symbols_from_file;
-use ticker_sniffer::{extract_tickers_from_text, DEFAULT_WEIGHTS};
+use ticker_sniffer::{
+    extract_tickers_from_text, ResultBiasAdjuster, DEFAULT_RESULT_BIAS_ADJUSTER_WEIGHTS,
+    DEFAULT_WEIGHTS,
+};
 
 fn benchmark_extract_tickers(c: &mut Criterion) {
     let symbols_map =
@@ -11,9 +14,17 @@ fn benchmark_extract_tickers(c: &mut Criterion) {
 
     let text = "AAPL is performing well, but MSFT is also a strong contender. There are also Amazon is another company.";
 
+    let result_bias_adjuster =
+        ResultBiasAdjuster::from_weights(DEFAULT_RESULT_BIAS_ADJUSTER_WEIGHTS);
+
     c.bench_function("extract_tickers", |b| {
         b.iter(|| {
-            extract_tickers_from_text(black_box(text), black_box(&symbols_map), DEFAULT_WEIGHTS)
+            extract_tickers_from_text(
+                black_box(text),
+                black_box(&symbols_map),
+                DEFAULT_WEIGHTS,
+                &result_bias_adjuster,
+            )
         })
     });
 }
