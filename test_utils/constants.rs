8fc519ac827073804ad65ba17fe8eb3df21996c1
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-12-10 16:08:59 -0600

    Migrate some test files

--- test_utils/constants.rs
+++ test_utils/constants.rs
@@ -1,7 +1,6 @@
 pub const DEFAULT_FALSE_NEGATIVE_WEIGHT: f32 = 2.0; // Higher penalty for missing tickers
 pub const DEFAULT_FALSE_POSITIVE_WEIGHT: f32 = 1.0; // Lower penalty for unexpected tickers
 
-// pub const TEST_FILES_DIRECTORY: &str = "tests/test_files";
-pub const TEST_FILES_DIRECTORY: &str = "tests/test_files_edge_cases_temp";
+pub const TEST_FILES_DIRECTORY: &str = "tests/test_files";
 
 pub const TEST_SYMBOLS_CSV_PATH: &str = "tests/test_data_files/test_symbols.csv";
