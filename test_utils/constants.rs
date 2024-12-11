a9852ca30d65a56150ab07d8ca4bca7bfb568746
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-12-11 13:13:28 -0600

    Add TODOs

--- test_utils/constants.rs
+++ test_utils/constants.rs
@@ -1,6 +1,9 @@
-pub const DEFAULT_FALSE_NEGATIVE_WEIGHT: f32 = 2.0; // Higher penalty for missing tickers
-pub const DEFAULT_FALSE_POSITIVE_WEIGHT: f32 = 1.0; // Lower penalty for unexpected tickers
+// TODO: Remove?
+// pub const DEFAULT_FALSE_NEGATIVE_WEIGHT: f32 = 2.0; // Higher penalty for missing tickers
+// pub const DEFAULT_FALSE_POSITIVE_WEIGHT: f32 = 1.0; // Lower penalty for unexpected tickers
 
+// TODO: Use path lib
 pub const TEST_FILES_DIRECTORY: &str = "tests/test_files";
 
+// TODO: Use path lib
 pub const TEST_SYMBOLS_CSV_PATH: &str = "tests/test_data_files/test_symbols.csv";
