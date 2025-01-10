b7e1ffd210fc12fb27f08dd72ef4fb939ba1afb6
Jeremy Harris <jeremy.harris@zenosmosis.com>
2025-01-10 16:43:11 -0600

    Respond with combined ticker frequencies

--- src/models/company_token_processor.rs
+++ src/models/company_token_processor.rs
@@ -125,7 +125,7 @@ impl<'a> CompanyTokenProcessor<'a> {
     }
 
     // TODO: Use Result type for output
-    pub fn process_text_doc(&mut self, text: &str) -> Result<Vec<TokenId>, Error> {
+    pub fn process_text_doc(&mut self, text: &str) -> Result<HashMap<TickerSymbol, usize>, Error> {
         // Tokenize the input text
         println!("Tokenizing...");
         let ticker_symbol_tokens = self.ticker_symbol_tokenizer.tokenize(text);
@@ -172,7 +172,7 @@ impl<'a> CompanyTokenProcessor<'a> {
         let ratio_exact_matches = self.calc_exact_ticker_symbol_match_ratio(&top_range_states);
 
         // Keep track of number of occurrences, per extracted symbol, for context stats
-        let top_range_state_counts =
+        let text_doc_ticker_frequencies =
             self.count_token_range_ticker_symbol_frequencies(&top_range_states);
 
         let query_ticker_symbols: Vec<TickerSymbol> = self
@@ -185,29 +185,29 @@ impl<'a> CompanyTokenProcessor<'a> {
         let unique_query_ticker_symbols = dedup_vector(&query_ticker_symbols);
 
         let unique_text_doc_ticker_symbols: Vec<TickerSymbol> =
-            top_range_state_counts.keys().cloned().collect();
+            text_doc_ticker_frequencies.keys().cloned().collect();
 
         // TODO: Keep track of same ticker symbol token IDs which are "consumed" by the text doc query
         // (as well as the number of occurrences), and taking into account the ratio of ticker symbol
         // token IDs to text doc tokens, determine whether to include these in the results
         println!(
-            "query_text_doc_token_ids: {:?}, query_text_doc_tokens: {:?}, query_ticker_symbols: {:?}, unique_query_ticker_symbols: {:?}, top_range_state_counts: {:?}, ratio_exact_matches: {}, match_threshold: {}",
-            query_text_doc_token_ids, self.token_mapper.get_tokens_by_ids(&query_text_doc_token_ids), &query_ticker_symbols, &unique_query_ticker_symbols, top_range_state_counts, ratio_exact_matches, self.config.threshold_ratio_exact_matches
+            "query_text_doc_token_ids: {:?}, query_text_doc_tokens: {:?}, query_ticker_symbols: {:?}, unique_query_ticker_symbols: {:?}, text_doc_ticker_frequencies: {:?}, ratio_exact_matches: {}, match_threshold: {}",
+            query_text_doc_token_ids, self.token_mapper.get_tokens_by_ids(&query_text_doc_token_ids), &query_ticker_symbols, &unique_query_ticker_symbols, text_doc_ticker_frequencies, ratio_exact_matches, self.config.threshold_ratio_exact_matches
         );
 
         // TODO: Filter out token range states less than a minimum score threshold (is this still necessary?)
 
-        let mut response_tokens: Vec<TokenId> = Vec::new();
+        // let mut response_tokens: Vec<TokenId> = Vec::new();
 
-        for (ticker_symbol, text_doc_occurrence_count) in top_range_state_counts {
-            let ticker_symbol_token_id =
-                self.token_mapper
-                    .get_token_id(&ticker_symbol)
-                    .expect(&format!(
-                        "Could not obtain token ID for symbol: {ticker_symbol}"
-                    ));
-            response_tokens.push(ticker_symbol_token_id);
-        }
+        // for (ticker_symbol, text_doc_occurrence_count) in &text_doc_ticker_frequencies {
+        //     let ticker_symbol_token_id =
+        //         self.token_mapper
+        //             .get_token_id(&ticker_symbol)
+        //             .expect(&format!(
+        //                 "Could not obtain token ID for symbol: {ticker_symbol}"
+        //             ));
+        //     response_tokens.push(ticker_symbol_token_id);
+        // }
 
         // TODO: Rename
         let query_tickers_not_in_text_doc: Vec<TickerSymbol> = unique_query_ticker_symbols
@@ -216,12 +216,20 @@ impl<'a> CompanyTokenProcessor<'a> {
             .filter(|symbol| !unique_text_doc_ticker_symbols.contains(symbol))
             .collect();
 
+        let query_ticker_frequencies =
+            self.count_ticker_symbol_frequencies(&query_tickers_not_in_text_doc);
+
+        let combined_ticker_frequencies = self.combine_ticker_symbol_frequencies(&[
+            text_doc_ticker_frequencies.clone(),
+            query_ticker_frequencies.clone(),
+        ]);
+
         println!(
-            "unique_text_doc_ticker_symbols: {:?}, unique_query_ticker_symbols: {:?}, query_tickers_not_in_text_doc: {:?}",
-            unique_text_doc_ticker_symbols, unique_query_ticker_symbols, query_tickers_not_in_text_doc
+            "unique_text_doc_ticker_symbols: {:?}, unique_query_ticker_symbols: {:?}, query_tickers_not_in_text_doc: {:?}, text_doc_ticker_frequencies: {:?}, query_ticker_frequencies: {:?}, combined_ticker_frequencies: {:?}",
+            unique_text_doc_ticker_symbols, unique_query_ticker_symbols, query_tickers_not_in_text_doc, text_doc_ticker_frequencies, query_ticker_frequencies, combined_ticker_frequencies
         );
 
-        Ok(response_tokens)
+        Ok(combined_ticker_frequencies)
 
         // for token_range_state in &token_range_states {
         //     println!(
@@ -254,6 +262,23 @@ impl<'a> CompanyTokenProcessor<'a> {
         // println!("Query token IDs: {:?}", query_text_doc_token_ids);
         // println!("Possible matches: {:?}", potential_token_id_sequences);
         // println!("Scores: {:?}", scores);
+    }
+
+    fn combine_ticker_symbol_frequencies(
+        &self,
+        ticker_symbol_frequency_hash_maps: &[HashMap<TickerSymbol, usize>],
+    ) -> HashMap<TickerSymbol, usize> {
+        let mut combined_ticker_frequencies: HashMap<TickerSymbol, usize> = HashMap::new();
+
+        for frequency_hash_map in ticker_symbol_frequency_hash_maps {
+            for (ticker_symbol, frequency) in frequency_hash_map {
+                *combined_ticker_frequencies
+                    .entry(ticker_symbol.clone())
+                    .or_insert(0) += frequency;
+            }
+        }
+
+        combined_ticker_frequencies
     }
 
     fn count_ticker_symbol_frequencies(
