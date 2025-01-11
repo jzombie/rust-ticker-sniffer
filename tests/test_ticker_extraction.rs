02766cdc288d9858d1ebf0a3744a54dc08c87cbe
Jeremy Harris <jeremy.harris@zenosmosis.com>
2025-01-11 15:05:28 -0600

    Show file testing progress

--- tests/test_ticker_extraction.rs
+++ tests/test_ticker_extraction.rs
@@ -12,15 +12,28 @@ mod tests {
 
     #[test]
     fn test_extract_tickers_from_multiple_files() {
+        println!("Testing ticker extractions...");
+
         // Directory containing the test files
         let test_dir = TEST_FILES_DIRECTORY;
 
         // Read all files in the directory
-        let files = read_dir(test_dir).expect("Failed to read test files directory");
+        let files = read_dir(test_dir)
+            .expect("Failed to read test files directory")
+            .collect::<Result<Vec<_>, _>>()
+            .expect("Failed to collect directory entries");
 
-        for file in files {
-            let file = file.expect("Failed to read file");
+        let total_files = files.len();
+
+        for (file_idx, file) in files.iter().enumerate() {
             let file_path = file.path();
+
+            println!(
+                "   -- {:?} ({} of {})",
+                file_path,
+                file_idx + 1,
+                total_files
+            );
 
             // Run the test for each file (if it is a file)
             if file_path.is_file() {
@@ -32,44 +45,3 @@ mod tests {
         }
     }
 }
-
-// TODO: Remove
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     #[test]
-//     fn test_extract_tickers_from_multiple_files() {
-//         // SAFETY: We need to ensure no data races while accessing CONTEXT_ATTENTION
-//         unsafe {
-//             if CONTEXT_ATTENTION.is_none() {
-//                 CONTEXT_ATTENTION = Some(ResultBiasAdjuster::from_weights(
-//                     DEFAULT_RESULT_BIAS_ADJUSTER_WEIGHTS,
-//                 ));
-//             }
-
-//             let result_bias_adjuster = CONTEXT_ATTENTION.as_ref().unwrap();
-
-//             // Directory containing the test files
-//             let test_dir = TEST_FILES_DIRECTORY;
-
-//             // Read all files in the directory
-//             let files = read_dir(test_dir).expect("Failed to read test files directory");
-
-//             for file in files {
-//                 let file = file.expect("Failed to read file");
-//                 let file_path = file.path();
-
-//                 // Run the test for each file (if it is a file)
-//                 if file_path.is_file() {
-//                     run_test_for_file(
-//                         file_path.to_str().unwrap(),
-//                         true,
-//                         DEFAULT_CONFIG,
-//                         result_bias_adjuster,
-//                     );
-//                 }
-//             }
-//         }
-//     }
-// }
