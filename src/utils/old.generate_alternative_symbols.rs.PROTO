1c05b16e0773209c9760ac8273a39d094ef2b5c7
Jeremy Harris <jeremy.harris@zenosmosis.com>
2025-01-12 09:05:15 -0600

    Delete old utils

--- src/utils/old.generate_alternative_symbols.rs.PROTO
+++
@@ -1,9 +0,0 @@
-pub fn generate_alternative_symbols(query: &str) -> Vec<String> {
-    let mut alternatives: Vec<String> = vec![query.to_uppercase()];
-    if query.contains('.') {
-        alternatives.push(query.replace('.', "-").to_uppercase());
-    } else if query.contains('-') {
-        alternatives.push(query.replace('-', ".").to_uppercase());
-    }
-    alternatives
-}
