c32653e277aee7f75e7127269aacd6b25329c1d1
Jeremy Harris <jeremy.harris@zenosmosis.com>
2025-01-13 10:32:53 -0600

    Comment-out prototype code

--- ticker-sniffer/src/lib.rs
+++ ticker-sniffer/src/lib.rs
@@ -11,20 +11,22 @@ pub use ticker_sniffer_common_lib::types::{
 };
 
 mod utils;
-use bincode;
-use serde::{Deserialize, Serialize};
 
+// TODO: Remove
+// use bincode;
+// use serde::{Deserialize, Serialize};
+//
 // Embed the bytes from the pre-generated binary file
-const DUMMY_GENERATED_BYTES: &[u8] = include_bytes!("./__dummy_generated__.bin");
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Entity {
-    x: f32,
-    y: f32,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct World(Vec<Entity>);
+// const DUMMY_GENERATED_BYTES: &[u8] = include_bytes!("./__dummy_generated__.bin");
+//
+// #[derive(Serialize, Deserialize, Debug)]
+// struct Entity {
+//     x: f32,
+//     y: f32,
+// }
+//
+// #[derive(Serialize, Deserialize, Debug)]
+// struct World(Vec<Entity>);
 
 pub fn extract_tickers_from_text(
     text: &str,
@@ -44,15 +46,16 @@ pub fn extract_tickers_from_text_with_custom_config(
     text: &str,
     company_symbols_list: &CompanySymbolList,
 ) -> Result<TickerSymbolFrequencyMap, Error> {
+    // TODO: Remove
     // Deserialize the embedded bytes
-    match bincode::deserialize::<World>(DUMMY_GENERATED_BYTES) {
-        Ok(world) => {
-            println!("Deserialized World: {:?}", world);
-        }
-        Err(err) => {
-            eprintln!("Failed to deserialize embedded data: {}", err);
-        }
-    }
+    // match bincode::deserialize::<World>(DUMMY_GENERATED_BYTES) {
+    //     Ok(world) => {
+    //         println!("Deserialized World: {:?}", world);
+    //     }
+    //     Err(err) => {
+    //         eprintln!("Failed to deserialize embedded data: {}", err);
+    //     }
+    // }
 
     let mut company_token_processor =
         CompanyTokenProcessor::new(document_token_processor_config, company_symbols_list);
