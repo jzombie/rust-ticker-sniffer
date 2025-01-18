02ac3a3a97d7d278b4d614d78d61236565013447
Jeremy Harris <jeremy.harris@zenosmosis.com>
2025-01-18 17:43:47 -0600

    Re-add `CODE_AUTOGEN_PREFIX`

--- shared/constants.rs
+++ shared/constants.rs
@@ -19,4 +19,6 @@ pub static TEST_RUNNER_VALIDATION_FILES_DIRECTORY: LazyLock<PathBuf> =
 pub static COMPANY_SYMBOL_CSV_FILE_PATH: LazyLock<PathBuf> =
     LazyLock::new(|| define_path!("data", "company_symbol_list.csv"));
 
+pub static CODE_AUTOGEN_PREFIX: &str = "__AUTOGEN__";
+
 pub static COMPRESSED_COMPANY_SYMBOL_FILE_NAME: &str = "company_symbol_list.csv.gz";
