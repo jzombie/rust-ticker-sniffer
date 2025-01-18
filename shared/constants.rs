7279d561226553c749aa4c0fdd1dcbe22bb17c27
Jeremy Harris <jeremy.harris@zenosmosis.com>
2025-01-18 17:29:43 -0600

    Allow unused constants

--- shared/constants.rs
+++ shared/constants.rs
@@ -1,3 +1,5 @@
+#![allow(unused)] // Ignore due to all constants not being utilized across all environemnts
+
 use std::path::PathBuf;
 use std::sync::LazyLock;
 
@@ -16,7 +18,5 @@ pub static TEST_RUNNER_VALIDATION_FILES_DIRECTORY: LazyLock<PathBuf> =
 
 pub static COMPANY_SYMBOL_CSV_FILE_PATH: LazyLock<PathBuf> =
     LazyLock::new(|| define_path!("data", "company_symbol_list.csv"));
-
-pub static CODE_AUTOGEN_PREFIX: &str = "__AUTOGEN__";
 
 pub static COMPRESSED_COMPANY_SYMBOL_FILE_NAME: &str = "company_symbol_list.csv.gz";
