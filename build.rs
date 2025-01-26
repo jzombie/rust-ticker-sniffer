ef7719e9ecd774eba2c3fadfafeacfef087bcdfb
Jeremy Harris <jeremy.harris@zenosmosis.com>
2025-01-26 00:55:55 -0600

    Switch to `embed-resources`

--- build.rs
+++ build.rs
@@ -2,11 +2,7 @@
 mod shared;
 use shared::constants::COMPANY_SYMBOL_CSV_FILE_PATH;
 
-use embed_bytes::write_byte_arrays;
-use flate2::write::GzEncoder;
-use flate2::Compression;
-use std::fs::File;
-use std::io::{self};
+use embed_resources::{Resource, ResourceContainer};
 use std::path::PathBuf;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -19,28 +15,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Set the output directory for generated files
     let output_rust_path = PathBuf::from("embed");
 
-    // Open the input CSV file
-    let mut input_file =
-        File::open(&*COMPANY_SYMBOL_CSV_FILE_PATH).expect("Could not open the input CSV file");
+    let mut resource_container = ResourceContainer::new();
 
-    // Create a buffer to hold the compressed data
-    let mut compressed_data = Vec::new();
+    resource_container.add_resource(
+        "COMPRESSED_COMPANY_SYMBOL_LIST_BYTE_ARRAY",
+        Resource::File(
+            COMPANY_SYMBOL_CSV_FILE_PATH
+                .to_str()
+                .expect("Path contains invalid UTF-8 characters")
+                .to_string(),
+        ),
+    );
 
-    // Compress the data with GzEncoder
-    {
-        let mut encoder = GzEncoder::new(&mut compressed_data, Compression::default());
-        io::copy(&mut input_file, &mut encoder).expect("Failed to compress the CSV file");
-        encoder.finish().expect("Failed to finalize compression");
-    }
-
-    // Write the compressed data to the `embed` directory
-    write_byte_arrays(
-        &output_rust_path,
-        vec![(
-            "COMPRESSED_COMPANY_SYMBOL_LIST_BYTE_ARRAY",
-            compressed_data.into(),
-        )],
-    )?;
+    resource_container.embed_all(&output_rust_path, true)?;
 
     println!("Files successfully written to {:?}", output_rust_path);
 
